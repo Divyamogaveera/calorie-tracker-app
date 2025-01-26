500e3ec6e07e26f0373701ec1a73d4954ddb56d4
Divyamogaveera <85350201+Divyamogaveera@users.noreply.github.com>
2025-01-26 11:14:20 +0530

    Update AppModal.js

--- src/components/AppModal/AppModal.js
+++ src/components/AppModal/AppModal.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppModal = ({ setOpenModal }) => {
   return (
     <div className="app__modal">
-      <h3>Calories must br bigger than zero and meal name can not be blank</h3>
+      <h3>Calories must be bigger than zero and meal name can not be blank</h3>
       <button className="btn__close__modal" onClick={() => setOpenModal(false)}>
         Close
       </button>
